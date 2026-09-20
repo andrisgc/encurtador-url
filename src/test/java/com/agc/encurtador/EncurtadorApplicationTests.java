2ae7d2f5cc027d127530ca1a33391074f18e3644
andrisgc <andrisgcastro4@gmail.com>
2026-09-20 19:59:46 -0300

    feat: adiciona estrutura base do spring

---
+++ src/test/java/com/agc/encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agc.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
